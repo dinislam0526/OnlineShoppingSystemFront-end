9b6381acd38fa0dfe9b9e2a8604aff638ba54dc4
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-01-11 10:59:12 +0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserHeaderComponent } from './common/user-header/user-header.component';
+import { UserFooterComponent } from './common/user-footer/user-footer.component';
+import { AdminHeaderComponent } from './common/admin-header/admin-header.component';
+import { AdminFooterComponent } from './common/admin-footer/admin-footer.component';
+import { UserHomeComponent } from './userPanel/user-home/user-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    UserHomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
