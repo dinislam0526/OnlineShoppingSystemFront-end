31ca629745b70cf4f8dd7dda536ad79be85048eb
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-01-12 13:05:48 +0600

    commit_12/01/23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './adminPanel/admin-home/admin-home.component';
 import { FontPageComponent } from './userPanel/font-page/font-page.component';
 
 import { LoginPageComponent } from './userPanel/login-page/login-page.component';
@@ -7,10 +8,15 @@ import { RegistrationPageComponent } from './userPanel/registration-page/registr
 import { UserHomeComponent } from './userPanel/user-home/user-home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'FontPageComponent' },
+  { path: '', pathMatch: 'full', redirectTo: '/userHome'
+ },
+  { path: 'userHome', component: UserHomeComponent },
+
   { path: 'FontPageComponent', component: FontPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'registration', component: RegistrationPageComponent }
+  { path: 'registration', component: RegistrationPageComponent },
+  { path: 'adminHome', component: AdminHomeComponent }
+
 ];
 
 
