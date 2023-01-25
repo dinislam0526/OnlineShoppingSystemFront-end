e5c7f79989d526f7b29efb1c9f5d25ccef4c228c
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-01-26 00:35:40 +0600

    commit_26/01/23

--- src/app/adminPanel/components/addproduct/addproduct.component.ts
+++ src/app/adminPanel/components/addproduct/addproduct.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/Category.model';
+import { Product } from '../../models/product.mode';
 import { ProductService } from '../../services/productService/product.service';
 
 @Component({
@@ -12,22 +13,44 @@ export class AddproductComponent implements OnInit {
   displayedColumns: string[] = ['Product ID', 'Category Name','Product Name','Product Price', 'Product Description', 'Product ImageName', 'Actions'];
   allCategory!: Category[];
 
+
   constructor(
-    public productService:ProductService
+    public productService: ProductService
   ) { }
 
   ngOnInit(): void {
-    this.getAllCategoryName();
+    // this.getAllCategoryName();
   }
 
-  getAllCategoryName(){
-    this.productService.getAllCategoryName().subscribe(
-      (data: Category[]) => {
-        this.allCategory = data
-      }
-    );
+
+  // getAllCategoryName(){
+  //   this.productService.getAllCategoryName().subscribe(
+  //     (data: Category[]) => {
+  //       this.allCategory = data
+  //     }
+  //   );
+  // }
+
+  createOrUpdateProduct() {
+    console.log(this.productService.currentProduct);
+    if ( this.productService.currentProduct.pro_id != null ) {
+      this.updateProduct(this.productService.currentProduct);
+    } else {
+      this.createProduct(this.productService.currentProduct);
+    }
   }
 
+  createProduct(product: Product) {
+    this.productService.createProduct(product).subscribe();
+  }
+
+  updateProduct(product: Product) {
+    this.productService.updateProduct(product).subscribe();
+  }
+
+  // clear() {
+  //   this.productService.currentProduct =new Product();
+  // }
 
 
 }
--- src/app/adminPanel/services/productService/product.service.ts
+++ src/app/adminPanel/services/productService/product.service.ts
@@ -1,21 +1,80 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { Category } from '../../models/Category.model';
 import { Product } from '../../models/product.mode';
+
+const headerOption = {
+  headers: new HttpHeaders({
+    'content-type': 'application/json'
+  })
+};
+
+const headerOption2 = {
+  headers: new HttpHeaders({
+    'content-type': 'text'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
- currentProduct:Product = new Product();
- dataUrl = 'http://localhost:8080/product';
+  dataUrl = 'http://localhost:8080/product';
 
-  constructor(private http:HttpClient) {  }
+  // dataUrl2 = 'http://localhost:8080/api/v1/pcategory';
 
-  getAllCategoryName(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.dataUrl+'/catnamelist');
+  // dataUrl3 = 'http://localhost:8080/api/v1/productwithcatname';
+
+
+  currentProduct: Product = new Product();
+  panelOpenState = false;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private refreshNeeded = new Subject<void>();
+
+  get refreshNeed() {
+    return this.refreshNeeded;
+  }
+
+  // getAllProduct(): Observable<Product[]> {
+  //   return this.http.get<Product[]>(this.dataUrl, headerOption);
+  // }
+
+  // getAllProductAndCategory():Observable<Object[]>{
+  //   return this.http.get<Object[]>(this.dataUrl, headerOption);
+  // }
+
+  // getAllCategoryName(): Observable<Category[]> {
+  //   return this.http.get<Category[]>(this.dataUrl+'/catnamelist', headerOption);
+  // }
+
+  // deleteProduct(pid: number): Observable<Product> {
+  //   return this.http.delete<Product>(this.dataUrl + '/' + pid, headerOption).pipe(
+  //     tap(() => {
+  //       this.refreshNeeded.next();
+  //     })
+  //   );
+  // }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.dataUrl+'/post', product, headerOption).pipe(
+      tap(() => {
+        this.refreshNeeded.next();
+      })
+    );
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(this.dataUrl + '/' + product.pro_id, product, headerOption).pipe(
+      tap(() => {
+        this.refreshNeeded.next();
+      })
+    );
   }
 
 }
