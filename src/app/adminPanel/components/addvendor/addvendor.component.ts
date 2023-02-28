397563a3ebccb74f342586e23565d2c37919340d
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-03-01 01:13:45 +0600

    commit_01/03/23

--- src/app/adminPanel/components/addvendor/addvendor.component.ts
+++ src/app/adminPanel/components/addvendor/addvendor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Vendor } from '../../models/Vendor.model';
+import { VendorService } from '../../services/vendorService/vendor.service';
 
 @Component({
   selector: 'app-addvendor',
@@ -7,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddvendorComponent implements OnInit{
 
-  displayedColumns: string[] = ['Category ID', 'Category Name', 'Category Description', 'Actions'];
+  displayedColumns: string[] = ['Vendor ID', 'Vendor Name', 'Vendor Description', 'Actions'];
+ 
+   dataSource!: MatTableDataSource<Vendor>;
+   @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
    msg="";
-  ngOnInit(): void {
+ 
+   constructor(
+     public vendorService: VendorService
+   ) { }
+ 
+   ngOnInit(): void {
+     this.getAllVendor();
+     this.vendorService.refreshNeed.subscribe(() => {
+       this.getAllVendor();
+     });
+      
+   }
+ 
+   togglePanel() {
+     this.vendorService.panelOpenState = !this.vendorService.panelOpenState
+   }
+ 
+   createOrUpdateVendor(currentVendor: Vendor) {
+ 
+     if (currentVendor.vendorId != null) {
+       this.updateVendor(currentVendor);
+     } else {
+       this.createVendor(currentVendor);
+     }
+ 
+     this.msg="Saved Successfully!!"
+   }
+ 
+   createVendor(ven: Vendor) {
+     this.vendorService.createVendor(ven).subscribe();
+   }
+ 
+   updateVendor(ven: Vendor) {
+     this.vendorService.updateVendor(ven).subscribe();
+   }
+ 
+   deleteVendor(venId: number) {
+     this.vendorService.deleteVendor(venId).subscribe();
+   }
+ 
+   editVendor(cat: Vendor) {
+     this.vendorService.currentVendor = Object.assign({}, cat);
+     this.togglePanel();
+   }
    
-  }
-  
+   getAllVendor(){
+     this.vendorService.getAllVendor().subscribe(
+       (data: Vendor[]) => {
+         this.dataSource= new MatTableDataSource (data);
+         this.dataSource.paginator = this.paginator;
+       }
+     );
+   }
+
+   clear(){
+    this.vendorService.currentVendor = new Vendor();
+   }
+
 }
--- src/app/adminPanel/components/createpurchase/createpurchase.component.ts
+++ src/app/adminPanel/components/createpurchase/createpurchase.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.mode';
+import { Vendor } from '../../models/Vendor.model';
+import { ProductService } from '../../services/productService/product.service';
+import { VendorService } from '../../services/vendorService/vendor.service';
 
 
 @Component({
@@ -7,13 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createpurchase.component.css']
 })
 export class CreatepurchaseComponent implements OnInit {
+  
   displayedColumns: string[] = ['Detail ID', 'Purchase ID', 'Product Name','Qauntity', 'Unit Price','SubTotal', 'Date', 'Actions'];
+
+  allVendor!:Vendor[];
+  allProduct!:Product[];
+
   constructor(
-   
+   private vendorService:VendorService,
+   private productService:ProductService
+
   ) { }
 
   ngOnInit(): void {
+    this.getAllVendor();
+    this.getAllProduct();
+
   }
+
+
+  getAllVendor(){
+   this.vendorService.getAllVendor().subscribe((data)=>{
+    this.allVendor = data;
+   })
+  }
+
+  getAllProduct(){
+    this.productService.getAllProduct().subscribe((data)=>{
+     this.allProduct = data;
+    })
+   }
+
 
  
   }
